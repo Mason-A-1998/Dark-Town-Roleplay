ca3f965fa2e4675b757bdb251601951d77515548
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-07-30 10:23:55 +0200

    init
    init

--- slashcommands/setups/logchannels.js
+++ slashcommands/setups/logchannels.js
@@ -40,7 +40,7 @@ module.exports.run = async (client, inter, channel) => {
         .setTitle("Ban logs")
         .setColor(`${client.ui.color}`)
         .setDescription("In dit kanaal worden alle verbanningen verstuurd!\n\n**Let erop!** Zorg ervoor dat leden dit kanaal niet kunnen zien.")
-        .setFooter("RG Moderation | ©️ 2022")
+        .setFooter("Dark Town RP | ©️ 2022")
 
     const bans = inter.guild.channels.cache.find(x => x.id === db.get(`banlogs_${inter.guild.id}`))
     bans.send({ embeds: [banlog] })
@@ -49,7 +49,7 @@ module.exports.run = async (client, inter, channel) => {
         .setTitle("Timeout logs")
         .setColor(`${client.ui.color}`)
         .setDescription("In dit kanaal worden alle timeout logs verstuurd!\n\n**Let erop!** Zorg ervoor dat leden dit kanaal niet kunnen zien.")
-        .setFooter("RG Moderation | ©️ 2022")
+        .setFooter("Dark Town RP | ©️ 2022")
 
     const timeout = inter.guild.channels.cache.find(x => x.id === db.get(`timeoutlogs_${inter.guild.id}`))
     timeout.send({ embeds: [timeoutlog] })
