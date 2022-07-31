d5ae3bc232154d8b4279681c883b9b7dcf40af66
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-01 00:21:57 +0200

    init
    init

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.on('ready', async () => {
     let guilds = client.guilds.cache.size;
     let users = client.users.cache.size
     console.log(`🔑 [STARTED] | ${client.user.username} loaded!`)
-    let statuses = [`Naar Burgers`, `Naar Bot`]
+    let statuses = [`Burgers`, `Bot`]
     setInterval(function () {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         client.user.setActivity(status, { type: "WATCHING" });
