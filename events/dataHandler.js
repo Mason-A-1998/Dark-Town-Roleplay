bb2d1b4e5e20684d43c75b5fec333fe19a362b62
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-01 18:49:34 +0200

    init
    init

--- events/dataHandler.js
+++ events/dataHandler.js
@@ -138,13 +138,13 @@ async function createCmd(client) {
                     name: 'kick-logs',
                     description: 'Tag het kanaal dat u wilt gebruiken voor de kick-logs.',
                     type: 'CHANNEL',
-                    required: true,
+                    required: false,
                 },
                 {
                     name: 'ban-logs',
                     description: 'Tag het kanaal dat u wilt gebruiken voor de ban-logs.',
                     type: 'CHANNEL',
-                    required: true,
+                    required: false,
                 },
                 {
                     name: 'timeout-logs',
