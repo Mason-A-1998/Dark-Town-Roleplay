820e02e300b77138f248ab885dbd9ffa63378409
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-01 18:42:40 +0200

    init
    init

--- slashcommands/setups/logchannels.js
+++ slashcommands/setups/logchannels.js
@@ -12,40 +12,19 @@ module.exports.run = async (client, inter, channel) => {
 
     if (!inter.member.permissions.has("ADMINISTRATOR")) return inter.reply({ embeds: [noPermissions], ephemeral: true })
 
-    const kickChannel = inter.options.getChannel('kick-logs');
-    const banChannel = inter.options.getChannel('ban-logs');
     const timeoutChannel = inter.options.getChannel('timeout-logs');
 
-    db.set(`kicklogs_${inter.guild.id}`, kickChannel.id)
-    db.set(`banlogs_${inter.guild.id}`, banChannel.id)
     db.set(`timeoutlogs_${inter.guild.id}`, timeoutChannel.id)
 
     const succesEmbed = new Discord.MessageEmbed()
         .setTitle("Succes")
         .setColor("GREEN")
-        .setDescription(`Het kanaal ${kickChannel} is succesvol ingesteld voor de kick logs!\nHet kanaal ${banChannel} is succesvol ingesteld voor de ban logs!\nHet kanaal ${timeoutChannel} is succesvol ingesteld voor de timeout logs!`)
+        .setDescription(`Het kanaal ${timeoutChannel} is succesvol ingesteld voor de timeout logs!`)
         .setTimestamp()
         .setFooter("Dark TOwn RP | ©️ 2022")
 
-    const kicklog = new Discord.MessageEmbed()
-        .setTitle("Kick logs")
-        .setColor(`${client.ui.color}`)
-        .setDescription("In dit kanaal worden alle kicks verstuurd!\n\n**Let erop!** Zorg ervoor dat leden dit kanaal niet kunnen zien.")
-        .setFooter("Dark Town RP | ©️ 2022")
 
-    const kicks = inter.guild.channels.cache.find(x => x.id === db.get(`kicklogs_${inter.guild.id}`))
-    kicks.send({ embeds: [kicklog] })
-
-    const banlog = new Discord.MessageEmbed()
-        .setTitle("Ban logs")
-        .setColor(`${client.ui.color}`)
-        .setDescription("In dit kanaal worden alle verbanningen verstuurd!\n\n**Let erop!** Zorg ervoor dat leden dit kanaal niet kunnen zien.")
-        .setFooter("Dark Town RP | ©️ 2022")
-
-    const bans = inter.guild.channels.cache.find(x => x.id === db.get(`banlogs_${inter.guild.id}`))
-    bans.send({ embeds: [banlog] })
-
-    const timeoutlog = new Discord.MessageEmbed()
+        const timeoutlog = new Discord.MessageEmbed()
         .setTitle("Timeout logs")
         .setColor(`${client.ui.color}`)
         .setDescription("In dit kanaal worden alle timeout logs verstuurd!\n\n**Let erop!** Zorg ervoor dat leden dit kanaal niet kunnen zien.")
