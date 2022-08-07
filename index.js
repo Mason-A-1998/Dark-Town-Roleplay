b57f4cbcd9ad94a68fb7cb67a5e8151f3492de32
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-07 20:09:06 +0200

    init
    init

--- index.js
+++ index.js
@@ -50,14 +50,8 @@ client.on('ready', async () => {
     let guilds = client.guilds.cache.size;
     let users = client.users.cache.size
     console.log(`🔑 [STARTED] | ${client.user.username} loaded!`)
-
     client.user.setPresence({ activities: [{ name: `Dark Town Roleplay`, type: `WATCHING` }], status: 'idle' });
-    setInterval(function () {
-        let status = statuses[Math.floor(Math.random() * statuses.length)];
-        client.user.setActivity(status, { type: "WATCHING" });
-    }, 10000)
     createCmd(client)
-
 });
 
 
