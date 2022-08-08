56ba25ef6a48b95ea2996b7f81213c8eefe389ec
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-08 13:27:30 +0200

    init
    init

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { Client, Intents, DiscordAPIError, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const discord = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS], allowedMentions: { parse: ['users', 'roles'] } });
-const fs = require('fs');
+const client = new Discord.Client({ 
+    intents: [
+       Intents.FLAGS.GUILD_INVITES, 
+       Intents.FLAGS.GUILDS, 
+       Intents.FLAGS.GUILD_MEMBERS, 
+       Intents.FLAGS.GUILD_MESSAGES, 
+       Intents.FLAGS.GUILD_MESSAGE_REACTIONS, 
+       Intents.FLAGS.GUILD_VOICE_STATES, 
+       Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+    ], 
+       allowedMentions: { parse: ['users', 'roles'] }
+    
+    });
+    
+       const fs = require('fs');
 const yaml = require('js-yaml')
 const { Permissions } = require('discord.js')
 
