a70c5eadc7ea4bc66eb40bb1a15cb7d1f8b05912
Mason-A-1998 <110051117+Mason-A-1998@users.noreply.github.com>
2022-08-08 15:00:53 +0200

    init
    init

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const discord = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS], allowedMentions: { parse: ['users', 'roles'] } });
 const fs = require('fs');
-const yml = require('js-yml')
 const { Permissions } = require('discord.js')
 
 function loadFile(file) { return myFile = yaml.load(fs.readFileSync(`${file}`, 'utf8')) }
